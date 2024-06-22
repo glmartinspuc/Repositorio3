49478679afc1f7d2e1e4312335844de832ca8f8d
Gustavo Martins <glmartins@sga.pucminas.br>
2024-06-22 13:09:57 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c__4__exc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double eleitores, brancos, nulos, validos;
+            double porcentagem_brancos, porcentagem_nulos, porcentagem_validos;
+
+            Console.WriteLine("Digite a quantidade de eleitores: ");
+                eleitores = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a quantidade de votos em branco: ");
+                brancos = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a quantidade de votos nulos: ");
+                nulos = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a quantidade de votos válidos: ");
+                validos = double.Parse(Console.ReadLine());
+
+            porcentagem_brancos = (brancos*100) / eleitores;
+            porcentagem_nulos = (nulos * 100) / eleitores;
+            porcentagem_validos = (validos * 100) / eleitores;
+
+            Console.WriteLine("O percentual de votos em branco são: "+ porcentagem_brancos+"%");
+            Console.WriteLine("O percentual de votos nulos são: "+ porcentagem_nulos+"%");
+            Console.WriteLine("O percentual de votos válidos são: "+ porcentagem_validos+"%");
+            Console.ReadKey();
+
+
+        }   
+    }
+}
